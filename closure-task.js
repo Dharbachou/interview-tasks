1f3cd129d070f20f429848dbc6e71b886160fde9
Dzmitry Harbachou <mitya.gorbachev.2013@mail.ru>
2023-08-01 22:21:10 +0300

    Update closure-task.js

--- closure-task.js
+++ closure-task.js
@@ -1,5 +1,26 @@
+//task
 for (var i = 0; i < 10; i++) {
   setTimeout(() => {
     console.log(i);
   }, 0);
+}
+
+//solution
+for (let i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 0);
+}
+
+for (var i = 0; i < 10; i++) {
+  setTimeout(function(){
+    console.log(i);
+  }.call(i), 0);
+}
+
+for (var i = 0; i < 10; i++) {
+  var callback = function(){
+    console.log(i);
+  }.bind(i);
+  setTimeout(callback, 0);
 }
